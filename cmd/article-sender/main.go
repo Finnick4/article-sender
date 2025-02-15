ad23ea91ef714c80bcb6d1bc233f913936ce5005
gallfeder <finnick4gallfeder@gmail.com>
2025-02-15 13:43:47 +0100

    Initial Codebase

--- cmd/article-sender/main.go
+++ cmd/article-sender/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Test!")
+
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("-> ")
+	text, _ := reader.ReadString('\n')
+	text = strings.Replace(text, "\n", "", -1)
+
+	sendWebhook(text)
 }
---
+++ cmd/article-sender/webhook-sender.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func sendWebhook(msg string) {
+	// TODO: Send the message as a post request!
+	fmt.Println("msg:", msg)
+}
