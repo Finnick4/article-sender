dbe47625d6caaf0c59534d8ea3866a5c674b1c86
gallfeder <finnick4gallfeder@gmail.com>
2025-02-15 17:32:42 +0100

    [MAIN] Improved directory finding

--- cmd/article-sender/main.go
+++ cmd/article-sender/main.go
@@ -10,14 +10,21 @@ import (
 func main() {
 	var directory string
 
+	// dotenv
+	makeEnv()
+
 	reader := bufio.NewReader(os.Stdin)
 
+	// get directory
 	if len(os.Args) < 2 {
-		println("There are no args!")
-
-		fmt.Print("What is the directory?\n-> ")
-		text, _ := reader.ReadString('\n')
-		directory = strings.Replace(text, "\n", "", -1)
+		// test for enviroment variables
+		if env.dir != "" {
+			directory = env.dir
+		} else {
+			fmt.Print("What is the directory?\n-> ")
+			text, _ := reader.ReadString('\n')
+			directory = strings.Replace(text, "\n", "", -1)
+		}
 	} else {
 		directory = os.Args[1]
 	}
